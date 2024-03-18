732bbcc1cb22f129fb09c905a17fd70c0854d1a7
Artem <artem.andreyev212@gmail.com>
2024-03-18 21:32:30 +0200

    Added ASCII Art, added some new modifications and improved code overall

--- RealEstate.cs
+++ RealEstate.cs
@@ -1,79 +1,134 @@
+// абстрактный класс нельзя использовать напрямую, а только в наследниках, нужен для того чтобы описать нечто абстрактное, какую то сущность без определения
+// protected объявляет метод или свойство защищенными (похоже на private) отличается тем, что доступен из любого места в текущем классе или в производных классах
+// virtual - для того чтобы иметь возможность переопределить какой-то метод, который находится в базовом классе, без virtual новую реализацию (перепись кода) мы сделать не можем
+// override - сочетается с virtual, для того чтобы в классе наследние сделать новую реализацию/переопределить метод базового класса
+
 using System;
 
-// Abstraktais klases apraksts
 public abstract class RealEstate
 {
-    // Adrese nekustamajam īpašumam
     protected string address;
-
-    // Platība kvadrātmetros
     protected double area;
+    protected DateTime listingStartTime;
+    protected DateTime listingEndTime;
 
-    // Konstruktors, lai inicializētu nekustamā īpašuma adresi un platību
-    public RealEstate(string address, double area)
+    public RealEstate(string address, double area, DateTime listingStartTime, DateTime listingEndTime)
     {
         this.address = address;
         this.area = area;
+        this.listingStartTime = listingStartTime;
+        this.listingEndTime = listingEndTime;
     }
 
-    // Abstraktā metode, kas jāpārdefinē apakšklasēs
+    public RealEstate(string address, double area) : this(address, area, DateTime.MinValue, DateTime.MinValue) { }
+
     public abstract void DisplayInfo();
 
-    // Abstraktā metode, kas jāpārdefinē apakšklasēs
     public abstract string GetDescription();
+
+    public abstract int GetCapacity();
+
+    public abstract void CalculatePricePerSquareMeter(double price);
+
+    public void SetListingStartTime(DateTime startTime)
+    {
+        this.listingStartTime = startTime;
+    }
+
+    public void SetListingEndTime(DateTime endTime)
+    {
+        this.listingEndTime = endTime;
+    }
+
+    public DateTime GetListingStartTime()
+    {
+        return this.listingStartTime;
+    }
+
+    public DateTime GetListingEndTime()
+    {
+        return this.listingEndTime;
+    }
 }
 
-// Klase "Māja", kas manto "Nekustamais īpašums"
 public class House : RealEstate
 {
-    // Istabu skaits
     private int numberOfRooms;
-    private string landType; // Pieliku landType kā privāto lauku
+    private string landType;
 
-    // Konstruktors, kas izsauc vecāko klases konstruktoru un inicializē istabu skaitu un zemes gabala tipu
+    public House(string address, double area, int numberOfRooms, string landType, DateTime listingStartTime, DateTime listingEndTime) : base(address, area, listingStartTime, listingEndTime)
+    {
+        this.numberOfRooms = numberOfRooms;
+        this.landType = landType;
+    }
+
     public House(string address, double area, int numberOfRooms, string landType) : base(address, area)
     {
         this.numberOfRooms = numberOfRooms;
         this.landType = landType;
     }
 
-    // Pārrakstīta metode, kas attēlo informāciju par māju
     public override void DisplayInfo()
     {
         Console.WriteLine("Māja atrodas adresē: " + address + ", ar kopējo platību " + area + " kvadrātmetri.");
         Console.WriteLine("Tajā ir " + numberOfRooms + " istabas.");
-        Console.WriteLine("Tas ir " + landType + " tips majā.");
+        Console.WriteLine("Tas ir " + landType);
+        Console.WriteLine("Izlikšanas sākuma laiks: " + listingStartTime);
+        Console.WriteLine("Izlikšanas beigu laiks: " + listingEndTime);
     }
 
-    // Pārdefinē abstrakto metodi, lai sniegtu aprakstu par māju
     public override string GetDescription()
     {
         return "Māja atrodas adresē: " + address + ", ar kopējo platību " + area + " kvadrātmetri. Tajā ir " + numberOfRooms + " istabas.";
     }
+
+    public override int GetCapacity()
+    {
+        return numberOfRooms * 2;
+    }
+
+    public override void CalculatePricePerSquareMeter(double price)
+    {
+        double pricePerSquareMeter = price / area;
+        Console.WriteLine("Cena par kvadrātmetru mājai ir: " + pricePerSquareMeter + " EUR.");
+    }
 }
 
-// Klase "Zemes gabals", kas manto "Nekustamais īpašums"
 public class LandPlot : RealEstate
 {
-    // Zemes gabala tips (piemēram, lauksaimniecības, dzīvojamais uc.)
     private string landType;
 
-    // Konstruktors, kas izsauc vecāko klases konstruktoru un inicializē zemes gabala tipu
+    public LandPlot(string address, double area, string landType, DateTime listingStartTime, DateTime listingEndTime) : base(address, area, listingStartTime, listingEndTime)
+    {
+        this.landType = landType;
+    }
+
     public LandPlot(string address, double area, string landType) : base(address, area)
     {
         this.landType = landType;
     }
 
-    // Pārrakstīta metode, kas attēlo informāciju par zemes gabalu
     public override void DisplayInfo()
     {
         Console.WriteLine("Zemes gabals atrodas adresē: " + address + ", ar kopējo platību " + area + " kvadrātmetri.");
         Console.WriteLine("Tas ir " + landType + " tips zemes.");
+        Console.WriteLine("Izlikšanas sākuma laiks: " + listingStartTime);
+        Console.WriteLine("Izlikšanas beigu laiks: " + listingEndTime);
     }
 
-    // Pārdefinē abstrakto metodi, lai sniegtu aprakstu par zemes gabalu
     public override string GetDescription()
     {
         return "Zemes gabals atrodas adresē: " + address + ", ar kopējo platību " + area + " kvadrātmetri. Tas ir " + landType + " tips zemes.";
+    }
+
+    public override int GetCapacity()
+    {
+        return 0;
+    }
+
+    public override void CalculatePricePerSquareMeter(double price)
+    {
+        double pricePerSquareMeter = price / area;
+        Console.WriteLine("Cena par kvadrātmetru zemes gabalam ir: " + pricePerSquareMeter + " EUR.");
     }
 }
--- main.cs
+++ main.cs
@@ -4,23 +4,109 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        // Izveidojam māju un zemes gabalu objektus
-        House myHouse = new House("Brīvības iela 65", 150, 5, "Dzīvokļis");
-        LandPlot myLandPlot = new LandPlot("Rīgas iela 56", 500, "Lauksaimniecības zeme");
+        bool runProgram = true;
 
-        // Attēlojam informāciju par māju un zemes gabalu, izmantojot objektu saites
-        Console.WriteLine("Informācija par māju:");
-        myHouse.DisplayInfo();
-        Console.WriteLine("Mājā var dzīvot " + myHouse.GetCapacity() + " cilvēki.");
+        while (runProgram)
+        {
+            Console.WriteLine("Izvēlieties, ko vēlaties aplūkot:");
+            Console.WriteLine("1. Māju");
+            Console.WriteLine("2. Zemes gabalu");
+            Console.WriteLine("3. Iziet");
 
-        Console.WriteLine("\nInformācija par zemes gabalu:");
-        myLandPlot.DisplayInfo();
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice) || (choice < 1 || choice > 3))
+            {
+                Console.WriteLine("Lūdzu, ievadiet derīgu izvēles numuru (1, 2 vai 3):");
+            }
 
-        // Aprēķinām un attēlojam cenu par kvadrātmetru nekustamajam īpašumam
-        double housePrice = 150000; // Piemēram, cena par māju
-        double landPlotPrice = 50000; // Piemēram, cena par zemes gabalu
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine(@"
+         (
+ 
+           )
+         ( _   _._
+          |_|-'_~_`-._
+       _.-'-_~_-~_-~-_`-._
+   _.-'_~-_~-_-~-_~_~-_~-_`-._
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    |  []  []   []   []  [] |
+    |           __    ___   |   
+  ._|  []  []  | .|  [___]  |_._._.
+  |=|________()|__|()_______|=|=|=|
+^^^^^^^^^^^^^^^ === ^^^^^^^^^^^^^^^
+    _______      ===   
+   <_4sale_>       === 
+      ^|^             ===
+       |                 ===");
+                    DateTime houseListingStartTime = new DateTime(2024, 3, 18);
+                    DateTime houseListingEndTime = new DateTime(2024, 3, 30);
 
-        myHouse.CalculatePricePerSquareMeter(housePrice);
-        myLandPlot.CalculatePricePerSquareMeter(landPlotPrice);
+                    House myHouse = new House("Brīvības iela 65", 150, 5, "Māja", houseListingStartTime, houseListingEndTime);
+                    myHouse.DisplayInfo();
+                    Console.WriteLine("Mājā var dzīvot " + myHouse.GetCapacity() + " cilvēki.");
+
+                    double housePrice = 150000;
+                    myHouse.CalculatePricePerSquareMeter(housePrice);
+                    break;
+
+                case 2:
+                    Console.WriteLine(@"
+     .-.                                    ,-.
+  .-(   )-.                              ,-(   )-.
+ (     __) )-.                        ,-(_      __)
+  `-(       __)                      (_    )  __)-'
+    `(____)-',                        `-(____)-'
+  - -  :   :  - -
+      / `-' \
+    ,    |   .
+         .                         _
+                                  >')
+               _   /              (\\         (W)
+              =') //               = \     -. `|'
+               ))////)             = ,-      \(| ,-
+              ( (///))           ( |/  _______\|/____
+~~~~~~~~~~~~~~~`~~~~'~~~~~~~~~~~~~\|,-'::::::::::::::
+            _                 ,----':::::::::::::::::
+         {><_'c   _      _.--':MJP:::::::::::::::::::
+__,'`----._,-. {><_'c  _-':::::::::::::::::::::::::::
+:.:.:.:.:.:.:.\_    ,-'.:.:.:.:.:.:.:.:.:.:.:.:.:.:.:
+.:.:.:.:.:.:.:.:`--'.:.:.:.:.:.:.:.:.:.:.:.:.:.:.:.:.
+.....................................................");
+                    DateTime landPlotListingStartTime = new DateTime(2024, 2, 15);
+                    DateTime landPlotListingEndTime = new DateTime(2024, 4, 20);
+
+                    LandPlot myLandPlot = new LandPlot("Rīgas iela 56", 500, "Lauksaimniecības zeme", landPlotListingStartTime, landPlotListingEndTime);
+                    myLandPlot.DisplayInfo();
+
+                    double landPlotPrice = 50000;
+                    myLandPlot.CalculatePricePerSquareMeter(landPlotPrice);
+                    break;
+
+                case 3:
+                    Console.WriteLine("Programma tiek aizvērta. Uz redzēšanos!");
+                    runProgram = false;
+                    break;
+            }
+
+            if (runProgram)
+            {
+                Console.WriteLine("\nVai vēlaties turpināt? (Jā / Nē)");
+                string continueChoice = Console.ReadLine().ToLower();
+
+                while (continueChoice != "jā" && continueChoice != "nē")
+                {
+                    Console.WriteLine("Lūdzu, ievadiet derīgu atbildi (Jā / Nē):");
+                    continueChoice = Console.ReadLine().ToLower();
+                }
+
+                if (continueChoice == "nē")
+                {
+                    Console.WriteLine("Programma tiek aizvērta. Uz redzēšanos!");
+                    runProgram = false;
+                }
+            }
+        }
     }
 }
